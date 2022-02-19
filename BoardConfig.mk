49b76bfa56297efdb0649bf44315ba295813ebe0
David <daviiideveloper@gmail.com>
2022-02-19 13:52:41 +0100

    LineageOS-recovery: Add kernel config
    *Builds

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j5nlte_eur_defconfig
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig
 
 # Ramdisk compression
 LZMA_RAMDISK_TARGETS := recovery
