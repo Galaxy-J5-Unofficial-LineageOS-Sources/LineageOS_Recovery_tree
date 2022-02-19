da6bdd053740e45856850fc4c6dd7de81a9ff4b2
David <daviiideveloper@gmail.com>
2022-02-19 12:49:07 +0100

    LineageOS-recovery: Define variant_defconfig and path for custom bootimg_mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive buildvariant=eng
-#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
-#TARGET_PREBUILT_DT := $(DEVICE_PATH)/prebuilt/dt.img
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -45,8 +44,8 @@ BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DT)
 BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_HEADER_ARCH := arm
-TARGET_KERNEL_SOURCE := kernel/samsung/j5nlte
-TARGET_KERNEL_CONFIG := j5nlte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
+TARGET_KERNEL_CONFIG := msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
 # Ramdisk compression
 LZMA_RAMDISK_TARGETS := recovery
