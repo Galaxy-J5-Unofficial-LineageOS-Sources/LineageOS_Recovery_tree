89a46b6d358b62f89c106fa36b782b057ae56d95
David <daviiideveloper@gmail.com>
2022-02-19 17:23:26 +0100

    SHRP: Use omni lunch products

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,15 +1,7 @@
-#
-# Copyright (C) 2021 The Android Open Source Project
-# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j5nlte.mk
-
+    $(LOCAL_DIR)/omni_j5nlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j5nlte-user \
-    lineage_j5nlte-userdebug \
-    lineage_j5nlte-eng
+    omni_j5nlte-user \
+    omni_j5nlte-userdebug \
+    omni_j5nlte-eng
